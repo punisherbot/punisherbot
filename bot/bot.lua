68f00d02fdb657fb99c08e18bdf86fde83f5cb8e
punisherbot <absam14339@yahoo.com>
2015-12-29 12:51:10 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {our_id},
+    sudo_users = {168398326},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
