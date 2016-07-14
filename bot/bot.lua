c409d4991b7a933fa2d70a7e735979e55c572ae6
omidgodarzi <omidgodarzi021@gmail.com>
2016-07-14 18:16:15 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {168398326},
+    sudo_users = {172313506},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
