24f6e933e84b4be5373d70d8ad5dbce0567e315d
yashartg <21157249+yasharsa@users.noreply.github.com>
2017-09-03 00:24:27 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -230,7 +230,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {our_id},
+    sudo_users = {391372093},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
